bd9eb0db1d813cd6a20e68d75eeaeeb8b9fc9641
lasyaswetha <47467184+lasyaswetha@users.noreply.github.com>
2019-12-15 18:24:35 +0530

    Create Super Reduced String.py

---
+++ Problem Solving/Super Reduced String.py
@@ -0,0 +1,13 @@
+s = input()
+while True:
+    for i in range(len(s)-1):
+        if s[i] == s[i+1]:
+            s = s[:i]+s[i+2:]
+            break
+    else:
+        break
+if(s):
+    print(s)
+else:
+    print("Empty String")
+    
